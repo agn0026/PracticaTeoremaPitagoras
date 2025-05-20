6b4bb218c329f88a35a06e2ffcd9f6e740d9a57d
agn0026 <agn0026@alu.medac.es>
2025-05-20 16:38:33 +0200

    3 commit depuracion

---
+++ PracticaTeoremaPitagoras/src/app/Principal.java
@@ -0,0 +1,19 @@
+package app;
+
+import clases.TrianguloRectangulo;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		TrianguloRectangulo tr1 = new TrianguloRectangulo(2, 4);
+		TrianguloRectangulo tr2 = new TrianguloRectangulo();
+		
+		double area1 = tr1.area();
+		double perimetro1 = tr1.perimetro();
+		
+		double area2 = tr2.area();
+		double perimetro2 = tr2.perimetro();
+
+	}
+
+}
